8f937e1a40e96827174eb4cd41699018555abf64
primemodel <adm@primemodel.com.br>
2026-08-05 16:17:50 -0300

    Enhance Puppeteer configuration in servidor.js
    Added additional Puppeteer arguments for improved performance and error handling.

--- servidor.js
+++ servidor.js
@@ -1,12 +1,13 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const express = require('express');
 const qrcode = require('qrcode');
+
 const client = new Client({
     authStrategy: new LocalAuth({
         clientId: 'prime-session'
     }),
     puppeteer: {
-        headless: true, // No Render deve ser true
+        headless: true,
         executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/opt/render/project/src/.cache/puppeteer/chrome/linux-146.0.7680.31/chrome-linux64/chrome',
         args: [
             '--no-sandbox',
@@ -17,7 +18,17 @@ const client = new Client({
             '--no-zygote',
             '--disable-gpu',
             '--single-process',
-            '--disable-extensions'
+            '--disable-extensions',
+            '--disable-infobars',
+            '--window-position=0,0',
+            '--ignore-certificate-errors',
+            '--ignore-certificate-errors-spki-list',
+            '--disable-features=IsolateOrigins,site-per-process',
+            '--aggressive-cache-discard',
+            '--disable-cache',
+            '--disable-application-cache',
+            '--disable-offline-load-stale-cache',
+            '--disk-cache-size=0'
         ]
     }
 });
